c0c5591bbf31e3a66be5cd6c5f22588ea08717d1
JingWay <guojingwei129@gmail.com>
2025-09-30 17:51:20 +0800

    删除修改操作

--- functions/nav.js
+++ functions/nav.js
@@ -1,5 +1,4 @@
 export async function onRequestGet(context) {
-    // 从 KV 获取 JSON
     const data = await context.env.GPage.get('navJson');
     return new Response(data || '[]', {
         headers: { 'Content-Type': 'application/json' }
@@ -7,7 +6,6 @@ export async function onRequestGet(context) {
 }
 
 export async function onRequestPost(context) {
-    // 更新导航数据（需要你自己做鉴权）
     const body = await context.request.text();
     await context.env.GPage.put('navJson', body);
     return new Response('OK');
